04b86b303bec42ea9e2b3b100ed24dc7f0efc84b
Anna Gulko <ingver.qa@gmail.com>
2024-06-23 22:09:20 +0300

    Добавлен тест

---
+++ src/test/java/helpers/Browserstack.java
@@ -0,0 +1,22 @@
+package helpers;
+
+
+import configs.BrowserstackDriverProvider;
+
+import static io.restassured.RestAssured.given;
+
+public class Browserstack extends BrowserstackDriverProvider {
+
+    public static String videoUrl(String sessionId) {
+        String url = String.format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
+
+        return given()
+                .auth().basic(authConfig.getUser(), authConfig.getKey())
+                .get(url)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().path("automation_session.video_url");
+    }
+}
